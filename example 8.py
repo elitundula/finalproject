ff01faa460656e6a04bead9860c56038d17aa48c
Sean Horne <shorne0516@gmail.com>
2025-06-26 14:51:52 -0400

    Delete example 8.py

--- example 8.py
+++
@@ -1,6 +0,0 @@
-user_input = input(1+1)
-print(eval(user_input)) #a safe way to use eval()
-
-user_in = "__import__ random.sys file"
-print(eval(user_in)) #unsafe way to use eval()
-#user_in could import possible malicious files into your code
